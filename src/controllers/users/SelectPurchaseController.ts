db3678aa7be289fa9d9c62d8501aa888dd64ceff
kaufon <kauanfvale@gmail.com>
2025-05-13 07:07:34 -0300

    add remove purchase

--- src/controllers/users/SelectPurchaseController.ts
+++ src/controllers/users/SelectPurchaseController.ts
@@ -1,54 +1,66 @@
-import type { ObjectId } from "mongodb";
 import type { IInput } from "../../core/interfaces";
 import type { UserModel } from "../../database/UserModel";
-import { ListAllUsersControler } from "./ListUserController";
 
 export class SelectPurchaseController {
-  private userModel: UserModel;
-  private input: IInput;
-  constructor(userModel: UserModel, input: IInput) {
-    this.userModel = userModel;
-    this.input = input;
-  }
-  async handle(userId: ObjectId): Promise<
-    | {
-      _id: ObjectId;
-      productId: ObjectId;
-      productName: string;
-      productPrice: number;
-      quantity: number;
-      totalPrice: number;
-    }
-    | undefined
-  > {
-    const purchases = await this.userModel.listPurchases(userId);
-    const filteredPurchases = purchases.map((purchase) => ({
-      ID: purchase._id.toHexString(),
-      Nome: purchase.productName,
-      Preço: purchase.productPrice,
-      Quantidade: purchase.quantity,
-      "Preço Total": purchase.totalPrice,
-    }));
-    if (filteredPurchases.length === 0) {
-      console.log("Nenhuma compra encontrada.");
-      return;
-    }
-    console.table(filteredPurchases);
+	private userModel: UserModel;
+	private input: IInput;
 
-    let selectedPurchase;
-    while (true) {
-      const selectedPurchaseId = await this.input.textInput(
-        "Digite o id da compra: ",
-      );
-      selectedPurchase = purchases.find(
-        (purchase) => purchase._id.toString() === selectedPurchaseId,
-      );
-      if (selectedPurchase) {
-        console.log(`Compra selecionada: ${selectedPurchase._id.toString()}`);
-        break;
-      }
-      console.log("Compra não encontrada. Tente novamente.");
-    }
-    return selectedPurchase;
-  }
+	constructor(userModel: UserModel, input: IInput) {
+		this.userModel = userModel;
+		this.input = input;
+	}
+
+	async handle(userId: string): Promise<
+		| {
+				_id: string;
+				productId: string;
+				productName: string;
+				productPrice: number;
+				quantity: number;
+				totalPrice: number;
+		  }
+		| undefined
+	> {
+		const purchases = await this.userModel.listPurchases(userId);
+
+		if (purchases.length === 0) {
+			console.log("Nenhuma compra encontrada.");
+			return;
+		}
+
+		const displayedPurchases = purchases.map((purchase, index) => ({
+			Nome: purchase.product_name,
+			Preço: purchase.product_price._intVal,
+			Quantidade: purchase.quantity,
+			"Preço Total": purchase.total_price._intVal,
+		}));
+		console.table(displayedPurchases);
+
+		let selectedIndex: number | undefined;
+		while (true) {
+			const input = await this.input.textInput("Digite o índice da compra: ");
+			const parsedIndex = Number(input);
+
+			if (
+				!Number.isNaN(parsedIndex) &&
+				parsedIndex >= 0 &&
+				parsedIndex < purchases.length
+			) {
+				selectedIndex = parsedIndex;
+				break;
+			}
+
+			console.log("Índice inválido. Tente novamente.");
+		}
+
+		const selectedPurchase = purchases[selectedIndex];
+		return {
+			_id: selectedPurchase.purchase_id,
+			productId: selectedPurchase.product_id,
+			productName: selectedPurchase.product_name,
+			productPrice: selectedPurchase.product_price._intVal,
+			quantity: selectedPurchase.quantity,
+			totalPrice: selectedPurchase.total_price._intVal,
+		};
+	}
 }
